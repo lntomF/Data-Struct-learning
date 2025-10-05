c61db1c64aafc9fc4c34ffe61e76a08066c3eafe
DGG <d2847077988@outlook.com>
2025-10-05 21:19:59 +0800

    The stack push

---
+++ Stack/Stack_arry.c
@@ -0,0 +1,48 @@
+#include <stdio.h> 
+#include <stdlib.h>
+
+#define MAX_SIZE 101
+int A[MAX_SIZE];
+int top = -1;
+
+void push(int x) //»Î’ª
+{
+    if(top == MAX_SIZE-1)
+    {
+        printf("The arrary is FUlly!");
+        return ;
+    }
+    A[++top] = x;
+}
+void pop() //≥ˆ’ª
+{
+    if( -1 == top)
+    {
+        printf("The Stack is Empty!");
+        return ;
+    }
+    top--;
+}
+
+int view_top()
+{
+    return A[top];
+}
+
+void print()
+{
+    printf("The Stack content:");
+    for(int i=0;i<=top;i++)
+    {
+        printf("%d ",A[i]);
+    }
+    printf("\n");
+}
+int main () {
+push (2);   print (); // stack 2
+push (5);   print (); // stack 5
+push (10);  print (); // stack 10
+pop ();     print (); // stack 10
+push (12);  print (); // stack 12
+return 0;
+}
